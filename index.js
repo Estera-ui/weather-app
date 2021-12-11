d693f7f67c1fb94bd0a42bb92aa7b3782b331f94
Estera-ui <94841300+Estera-ui@users.noreply.github.com>
2021-12-11 10:11:54 +0000

    removed the celsius and fahrenheit

--- index.js
+++ index.js
@@ -87,31 +87,6 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-  
-} 
-
- function displayCelsiusTemperature(event) {
-   event.preventDefault();
-   let temperatureElement = document.querySelector("#temperature");
-   temperatureElement.innerHTML = Math.round(celsiusTemperature);
- }
-
-
-let cesiusTemperature = null;
-
-
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
 
 
 searchCity("Birmingham");
